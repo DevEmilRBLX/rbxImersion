b29424c89683bfd64a4717cc2259e7645d3e3cd3
Emil <scherfem@gmail.com>
2026-04-03 23:50:09 +0200

    Added camera to loader, forgot

--- rbxImmersion/rbxImmersion.lua
+++ rbxImmersion/rbxImmersion.lua
@@ -1,13 +1,16 @@
 local Components = script.Parent.Components
 
 type ScreenBorderModule = typeof(require(Components.screenBorder))
+type CameraModule = typeof(require(Components.Camera))
 
 type Modules = {
-	screenBorder: ScreenBorderModule
+	screenBorder: ScreenBorderModule,
+	Camera: CameraModule
 }
 
 local rbxImmersion: Modules = {
 	screenBorder = require(Components.screenBorder),
+	Camera = require(Components.Camera),
 }
 
 return rbxImmersion
